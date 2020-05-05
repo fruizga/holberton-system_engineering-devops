4dee12e49a0bf5e7131f60900e043201ba814c2b
fruizga <ruizgallego@gmail.com>
2020-05-04 23:47:04 -0500

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Exports data in JSON format"""
+"""xport data in the JSON format"""
 import json
 import requests
+
 
 if __name__ == "__main__":
     """export JSON"""
     u = requests.get("https://jsonplaceholder.typicode.com/users").json()
     url2 = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    a_Dict = {}
+    a_dict = {}
     for i in u:
-        ar1 = i.get("id")
-        Name = i.get('username')
-        for j in url2:
-            if (j.get('userId') == int(ar1)):
-                dict = {}
-                dict['task'] = i.get('title')
-                dict['completed'] = i.get('completed')
-                dict['username'] = Name
-                dict.append(dictionary)
-        a_Dict[ar1] = dict
+        Name = [j for j in url2 if j.get('userId') == i.get('id')]
+        Name = [{'username': i.get('username'), 'task': j.get('title'),
+                 'completed': j.get('completed')} for j in Name]
+        a_dict[str(i.get('id'))] = Name
     with open("todo_all_employees.json", 'w') as JSONfile:
-        json.dump(a_Dict, JSONfile)
+        json.dump(a_dict, JSONfile)
