4742877d00591c53677c1fb41f5fa02c0ac5f3e8
fruizga <ruizgallego@gmail.com>
2020-05-04 21:20:09 -0500

    pep8 style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,10 +8,10 @@ import csv
 if __name__ == "__main__":
     """export CSV"""
     ar1 = argv[1]
-    url1 = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                     format(ar1)).json()
-    url2 = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(ar1)).json()
+    url1 = requests.get("https://jsonplaceholder.typicode.com/users/{}\
+    ".format(ar1)).json()
+    url2 = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}\
+    ".format(ar1)).json()
     with open("{}.csv".format(ar1), 'w') as CVSfile:
         result = writer(CVSfile, delimiter=',', quoting=QUOTE_ALL)
         for i in url2:
